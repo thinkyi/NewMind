fda3c054960d6cefed235e20ff65162be8df2c73
yianbeyond <yianbeyond@qq.com>
2014-05-17 10:23:10 +0800

    update

---
+++ trunk/ThinkYi.Service/Impl/UserService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.Practices.Unity;
+using System.Data.Entity;
+using ThinkYi.Domain;
+using ThinkYi.Data.Infrastructure;
+using ThinkYi.Data.Repositories;
+
+namespace ThinkYi.Service.Impl
+{
+    public class UserService : IUserService
+    {
+        [Dependency]
+        public IUserRepository UserRepository { get; set; }
+        [Dependency]
+        public IUnitOfWork UnitOfWork { get; set; }
+
+        public User GetUser(int id)
+        {
+            return UserRepository.GetByID(id);
+        }
+        public IQueryable<User> GetUsers()
+        {
+            return UserRepository.Entities;
+        }
+        public void EditUser(User user)
+        {
+            UserRepository.Update(user);
+            UnitOfWork.Commit();
+        }
+    }
+}
